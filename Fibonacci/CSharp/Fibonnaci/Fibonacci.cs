a523f2771752f4f8b077133328327d32cbf1b427
BoredTweak <alex@boredtweak.com>
2018-08-19 06:51:10 -0700

    Initial Fibonacci commit

---
+++ Fibonacci/CSharp/Fibonnaci/Fibonacci.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Fibonnaci
+{
+    public class Fibonacci
+    {
+        public static int CalculateFib(int n)
+        {
+            if (n < 2)
+            {
+                return n;
+            }
+
+            var output = CalculateFib(n - 2) + CalculateFib(n - 1);
+            Console.WriteLine($"Input {n} - Output {output}");
+            return output;
+        }
+    }
+}
---
+++ Fibonacci/CSharp/Fibonnaci/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Fibonnaci
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Fibonacci.CalculateFib(50);
+            Console.ReadLine();
+        }
+    }
+}
